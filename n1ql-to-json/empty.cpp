f2b5aeb4af4860f89f31fbfb524374b8be5068b7
Jens Alfke <jens@couchbase.com>
2019-04-16 16:13:15 -0700

    N1QL source code cleanup
    Copyright notices, formatting, etc.

--- n1ql-to-json/empty.cpp
+++ n1ql-to-json/empty.cpp
@@ -1,10 +1,5 @@
 //
 //  empty.cpp
-//  n1ql_to_json
-//
-//  Created by Jens Alfke on 4/9/19.
-//  Copyright © 2019 Couchbase. All rights reserved.
-//
 
 #include <stdio.h>
 
--- n1ql-to-json/n1ql_to_json.cc
+++ n1ql-to-json/n1ql_to_json.cc
@@ -1,15 +1,25 @@
 //
-//  n1ql_2_json.cc
-//  N1QL to JSON
+//  n1ql_to_json.cc
 //
-//  Created by Jens Alfke on 4/4/19.
-//  Copyright © 2019 Couchbase. All rights reserved.
+// Copyright (c) 2019 Couchbase, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #include "n1ql_to_json.h"
 #include "fleece/Fleece.hh"
 #include "fleece/Mutable.hh"
-#include "n1ql_parser.h"
+#include "n1ql_parser.hh"
 
 using namespace std;
 using namespace fleece;
