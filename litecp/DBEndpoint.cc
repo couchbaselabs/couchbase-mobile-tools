64e5566562a9507574388b78af9b5d8d2a549503
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-02-16 16:20:05 -0800

    CBL-4202 : Add User-Agent to replicator params
    * Added User-Agent to the replicator params for the replicator used by cblite tool. The User-Agent is required by the AWS Capella.
    
    * Update LiteCore to the latest lithium branch.

--- litecp/DBEndpoint.cc
+++ litecp/DBEndpoint.cc
@@ -379,6 +379,14 @@ C4ReplicatorParameters DbEndpoint::replicatorParameters(C4ReplicatorMode push, C
             enc.writeKey(slice(kC4ReplicatorOptionRootCerts));
             enc.writeData(_rootCerts);
         }
+        // User-Agent (Required by AWS Capella: CBL-4202)
+        enc.writeKey(slice(kC4ReplicatorOptionExtraHeaders));
+        enc.beginDict();
+        enc.writeKey(slice("User-Agent"));
+        alloc_slice version = c4_getVersion();
+        enc.writeString(format("cblite/%.*s", SPLAT(version)));
+        enc.endDict();
+        
         enc.endDict();
         _options = enc.finish();
         params.optionsDictFleece = _options;
