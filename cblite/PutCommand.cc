c0e900e75d2d00471bc3805d849080c7542ac786
Jens Alfke <jens@couchbase.com>
2023-08-10 17:20:58 -0700

    rm: added --purge flag

--- cblite/PutCommand.cc
+++ cblite/PutCommand.cc
@@ -26,7 +26,7 @@ using namespace litecore;
 
 class PutCommand : public CBLiteCommand {
 public:
-    enum PutMode {kPut, kUpdate, kCreate, kDelete};
+    enum PutMode {kPut, kUpdate, kCreate, kDelete, kPurge};
 
 
     PutCommand(CBLiteTool &parent, PutMode mode)
@@ -36,22 +36,29 @@ public:
 
 
     void usage() override {
-        writeUsageCommand("put", true, "DOCID \"JSON\"");
-        cerr <<
-        "  Updates a document.\n"
-        "    --create : Document must not exist\n"
-        "    --delete : Deletes the document (and JSON is optional); same as `rm` subcommand\n"
-        "    --update : Document must already exist\n"
-        "    -- : End of arguments (use if DOCID starts with '-')\n"
-        "    " << it("DOCID") << " : Document ID\n"
-        "    " << it("JSON") << " : Document body as JSON (JSON5 syntax allowed.) Must be quoted.\n"
-        ;
-
-        writeUsageCommand("rm", false, "DOCID");
-        cerr <<
-        "  Deletes a document. (Same as `put --delete`)\n"
-        "    " << it("DOCID") << " : Document ID\n"
-        ;
+        switch (_putMode) {
+            default:
+                writeUsageCommand("put", true, "DOCID \"JSON\"");
+                cerr <<
+                "  Updates a document.\n"
+                "    --create : Document must not exist\n"
+                "    --delete : Deletes the document (and JSON is optional); same as `rm` subcommand\n"
+                "    --purge  : Purges the document (deletes without leaving a tombstone)\n"
+                "    --update : Document must already exist\n"
+                "    -- : End of arguments (use if DOCID starts with '-')\n"
+                "    " << it("DOCID") << " : Document ID\n"
+                "    " << it("JSON") << " : Document body as JSON (JSON5 syntax allowed.) Must be quoted.\n"
+                ;
+                break;
+            case kDelete:
+                writeUsageCommand("rm", true, "DOCID");
+                cerr <<
+                "  Deletes a document. (Same as `put --delete`)\n"
+                "    --purge  : Purges the document (doesn't leave a tombstone)\n"
+                "    " << it("DOCID") << " : Document ID\n"
+                ;
+                break;
+        }
     }
 
 
@@ -61,12 +68,13 @@ public:
             {"--create", [&]{_putMode = kCreate;}},
             {"--update", [&]{_putMode = kUpdate;}},
             {"--delete", [&]{_putMode = kDelete;}},
+            {"--purge",  [&]{_putMode = kPurge;}},
         });
         openWriteableDatabaseFromNextArg();
 
         string docID = nextArg("document ID");
         string json5;
-        if (_putMode != kDelete)
+        if (_putMode < kDelete)
             json5 = restOfInput("document body as JSON");
         endOfArgs();
 
@@ -75,53 +83,63 @@ public:
         if (!t.begin(&error))
             fail("Couldn't open database transaction");
 
-        c4::ref<C4Document> doc = c4coll_getDoc(collection(), slice(docID), false, kDocGetAll, &error);
-        if (!doc)
-            fail("Couldn't read document", error);
-        bool existed = (doc->flags & kDocExists) != 0
-                    && (doc->selectedRev.flags & kRevDeleted) == 0;
-        if (!existed && (_putMode == kUpdate || _putMode == kDelete)) {
-            if (doc->flags & kDocExists)
-                fail("Document is already deleted");
-            else
-                fail("Document doesn't exist");
-        }
-        if (existed && _putMode == kCreate)
-            fail("Document already exists");
-
-        alloc_slice body;
-        if (_putMode != kDelete) {
-            FLStringResult errMsg;
-            alloc_slice json = FLJSON5_ToJSON(slice(json5), &errMsg, nullptr, nullptr);
-            if (!json) {
-                string message = string(alloc_slice(errMsg));
-                FLSliceResult_Release(errMsg);
-                fail("Invalid JSON: " + message);
+        c4::ref<C4Document> doc;
+        bool existed = false;
+        if (_putMode == kPurge) {
+            if (!c4coll_purgeDoc(collection(), slice(docID), &error))
+                fail("Couldn't purge document", error);
+        } else {
+            doc = c4coll_getDoc(collection(), slice(docID), false, kDocGetAll, &error);
+            if (!doc)
+                fail("Couldn't read document", error);
+            existed = (doc->flags & kDocExists) != 0
+            && (doc->selectedRev.flags & kRevDeleted) == 0;
+            if (!existed && (_putMode == kUpdate || _putMode == kDelete)) {
+                if (doc->flags & kDocExists)
+                    fail("Document is already deleted");
+                else
+                    fail("Document doesn't exist");
             }
-            body = c4db_encodeJSON(_db, json, &error);
-            if (!body)
-                fail("Couldn't encode body", error);
+            if (existed && _putMode == kCreate)
+                fail("Document already exists");
+
+            alloc_slice body;
+            if (_putMode != kDelete) {
+                FLStringResult errMsg;
+                alloc_slice json = FLJSON5_ToJSON(slice(json5), &errMsg, nullptr, nullptr);
+                if (!json) {
+                    string message = string(alloc_slice(errMsg));
+                    FLSliceResult_Release(errMsg);
+                    fail("Invalid JSON: " + message);
+                }
+                body = c4db_encodeJSON(_db, json, &error);
+                if (!body)
+                    fail("Couldn't encode body", error);
+            }
+
+            doc = c4doc_update(doc, body, (_putMode == kDelete ? kRevDeleted : 0), &error);
+            if (!doc)
+                fail("Couldn't save document", error);
         }
-
-        doc = c4doc_update(doc, body, (_putMode == kDelete ? kRevDeleted : 0), &error);
-        if (!doc)
-            fail("Couldn't save document", error);
-
         if (!t.commit(&error))
             fail("Couldn't commit database transaction", error);
 
         const char *verb;
-        if (_putMode == kDelete)
-            verb = "Deleted";
-        else if (existed)
-            verb = "Updated";
-        else
-            verb = "Created";
-        string revID = slice(doc->selectedRev.revID).asString();
-        if (revID.size() > 10)
-            revID.resize(10);
-        cout << verb << " `" << docID
-             << "`, new revision " << revID << " (sequence " << doc->sequence << ")\n";
+        if (_putMode == kPurge) {
+            cout << "Purged `" << docID << "`\n";
+        } else {
+            if (_putMode == kDelete)
+                verb = "Deleted";
+            else if (existed)
+                verb = "Updated";
+            else
+                verb = "Created";
+            string revID = slice(doc->selectedRev.revID).asString();
+            if (revID.size() > 10)
+                revID.resize(10);
+            cout << verb << " `" << docID << "`, new revision " << revID
+                 << " (sequence " << doc->sequence << ")\n";
+        }
     }
 
 private:
