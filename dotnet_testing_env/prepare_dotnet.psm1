fb645c27d1905dc78152c6a746870513aa2e53b2
Jim Borden <jim.borden@couchbase.com>
2026-05-09 10:27:37 +0900

    Update prepare_dotnet.psm1
    Segregate .NET installs

--- dotnet_testing_env/prepare_dotnet.psm1
+++ dotnet_testing_env/prepare_dotnet.psm1
@@ -1,8 +1,6 @@
-$env:DOTNET_ROOT="$env:LOCALAPPDATA\Microsoft\dotnet"
-
 function Banner {
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]
         $Text
     )
@@ -17,12 +15,18 @@ function Install-DotNet {
         [string]
         $Version = "8.0"
     )
-    if(-Not (Test-Path .\dotnet-install.ps1)) {
+    
+    if (-not $env:DOTNET_ROOT) {
+        Write-Error "Error: DOTNET_ROOT environment variable is not set."
+        exit 1
+    }
+
+    if (-Not (Test-Path .\dotnet-install.ps1)) {
         Invoke-WebRequest https://dot.net/v1/dotnet-install.ps1 -OutFile dotnet-install.ps1
     }
 
     Banner -Text "Installing .NET SDK $Version"
-    .\dotnet-install.ps1 -c $Version
+    .\dotnet-install.ps1 -c $Version -InstallDir $env:DOTNET_ROOT
 }
 
 function Install-DotNetRuntime {
@@ -31,21 +35,26 @@ function Install-DotNetRuntime {
         [string]
         $Version = "8.0"
     )
-    if(-Not (Test-Path .\dotnet-install.ps1)) {
+
+    if (-not $env:DOTNET_ROOT) {
+        Write-Error "Error: DOTNET_ROOT environment variable is not set."
+        exit 1
+    }
+
+    if (-Not (Test-Path .\dotnet-install.ps1)) {
         Invoke-WebRequest https://dot.net/v1/dotnet-install.ps1 -OutFile dotnet-install.ps1
     }
 
     Banner -Text "Installing .NET Runtime $Version"
-    .\dotnet-install.ps1 -c $Version -Runtime dotnet
+    .\dotnet-install.ps1 -c $Version -Runtime dotnet -InstallDir $env:DOTNET_ROOT
 }
 
 function Install-XHarness {
+    if (-not $env:DOTNET_ROOT) {
+        Write-Error "Error: DOTNET_ROOT environment variable is not set."
+        exit 1
+    }
+
     Banner -Text "Installing XHarness"
     & $env:DOTNET_ROOT\dotnet tool install --global --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-eng/nuget/v3/index.json Microsoft.DotNet.XHarness.CLI --version "8.0.0-prerelease*"
-}
-
-function Install-Maui {
-    Banner -Text "Installing MAUI workload"
-
-    & $env:DOTNET_ROOT\dotnet workload install maui
 }
